682ef911585badd8c66a44832d8fd7dcc165db64
Rawipol Wajanavisit <oakraw@gmail.com>
2020-08-31 16:30:04 +0700

    fix babel

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-require('babel-register');
-require('./index.js');
+import app from './app.js';
+import http from 'http';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_END_POINT,  {
+  useNewUrlParser: true
+})
+
+const db = mongoose.connection
+
+db.on("error", (err) => {
+    console.log("Cannot connect DB");
+})
+
+//create server by node
+http.createServer(app).listen(3000, () => {
+    console.log("server status : running");
+    console.log(`run on port : 3000`);
+});
+
+// app.get('/boss/:userId',bossController)
+
